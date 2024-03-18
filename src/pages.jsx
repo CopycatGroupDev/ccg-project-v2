601679e1c786fc9c340ec5f2eaf50137bc04f0da
NAKS Chadiii <selfdiaz@gmail.com>
2024-03-18 09:29:49 +0100

    Update

--- src/pages.jsx
+++ src/pages.jsx
@@ -44,18 +44,18 @@ export const Page = ({ header, title, Body }) => {
   }, [])
 
   return (
-    <><>
+    <>
       {<style>{`body{background:rgba(0,0,0,0.025);--thickness: 3px; --page-color: ${header.color}; overflow-x:hidden;} .colorPage{background-color: var(--page-color);}`}</style>}
       {<Header {...header} modal={{ opener: setMO, select: setMS, default: setMD }} />}
-      {Body && <Body modal={{ opener: setMO, select: setMS, default: setMD }} alertSetter={alertSetter} alert={alert} color={header.color} />}
+      {Body && <Body modal={{ opener: setMO, select: setMS, default: setMD }} alertSetter={alertSetter} alert={alert} color={header.color} /> }
       {<Footer refF={footer} />}
       {scroll > 100 && <button className="fixed z-[999] shadow-md shadow-black/30 bottom-0 right-0 rounded-full py-3 px-5 bg-[#0061ad] hover:bg-white text-white hover:text-[#0061ad] m-6 flex items-center gap-2 justify-center" onClick={() => scrollTo(0, 0)} style={{ bottom: bottomBtnScrTop }}>
-        <svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} fill="currentColor" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z" /> <path fillRule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" /> </svg>
-        <span>Retour en haut</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} fill="currentColor" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z" /> <path fillRule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" /> </svg>
+          <span>Retour en haut</span>
       </button>}
       {scroll > 100 && <button className="fixed z-[999] shadow-md shadow-black/30 bottom-0 right-0 rounded-full py-3 px-5 bg-[#0061ad] hover:bg-white text-white hover:text-[#0061ad] m-6 flex items-center gap-2 justify-center" onClick={() => scrollTo(0, 0)} style={{ bottom: bottomBtnScrTop }}>
-        <svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} fill="currentColor" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z" /> <path fillRule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" /> </svg>
-        <span>Retour en haut</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} fill="currentColor" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z" /> <path fillRule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" /> </svg>
+          <span>Retour en haut</span>
       </button>}
       {(body.width < 1024) && <button className="w-full py-3 px-4 bg-white text-[#0061ad] -m-12 flex items-center gap-2 justify-center" onClick={() => scrollTo(0, 0)}>
         <svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} fill="currentColor" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z" /> <path fillRule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" /> </svg>
@@ -63,7 +63,7 @@ export const Page = ({ header, title, Body }) => {
       </button>}
       {modalOpened && <Modal options={modalSelect} selected={modalDefault} openedSetter={setMO} alert={alert} alertSetter={alertSetter} />}
       {<div className='p-16 flex flex-col gap-2 fixed right-0 top-0'>{alert.map(({ text, color }, i) => <motion.div key={i} className='px-2.5 py-1.5 text-base rounded text-white shadow-md' style={{ background: color }} transition={{ delay: 3, duration: 0.5 }} animate={{ display: 'none' }}>{text}</motion.div>)}</div>}
-    </><iframe src="https://embedsocial.com/api/pro_hashtag/dac193a216549aa1b02261075be5c7c493283a43" width="900px" height="1200px" frameBorder="0" marginHeight="0" marginWidth="0"></iframe></>
+    </>
   )
 }
 
